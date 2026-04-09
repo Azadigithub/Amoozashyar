43d2c2cf5413c6ba8d6cd2cd37aee3b46ea2a93c
Ali <aliazadigithub@gmail.com>
2026-04-09 23:39:10 +0330

    Add (style & responsive): Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import { useRoutes } from "react-router-dom";
-const routes = [{ path: "/", element: <h1>Landing</h1> }];
+import Login from "./Pages/Login";
+const routes = [
+  { path: "/", element: <h1>Landing</h1> },
+  { path: "/Login", element: <Login/> },
+];
 function App() {
   const router = useRoutes(routes);
   return (
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="font-Vazir flex-center flex-col relative">
+      <div className="flex-center flex-col static left-[200px] w-[400px] tablet:absolute bg-white rounded-[10px] p-[10px]">
+        <img src="/Images/iaun-logo.png" alt="" className="w-[80%] mx-auto" />
+        <div className=" ">
+          <form className="flex-center flex-col gap-[10px] mt-[20px] max-w-[300px] mx-auto">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="نام کاربری"
+              className="w-full border-gray-300 border-2 py-[5px] px-[10px] rounded-[5px]"
+            />
+            <input
+              type="password"
+              name=""
+              id=""
+              placeholder="کلمه عبور"
+              className="w-full border-gray-300 border-2 py-[5px] px-[10px] rounded-[5px]"
+            />
+            <button className="bg-blue-500 text-white p-[10px] rounded-[5px] hover:cursor-pointer">
+              ورود به سایت
+            </button>
+          <hr className="w-[100%]" />
+          <h3>نام کاربری: شماره دانشجویی</h3>
+          <h3>رمز عبور: کد ملی</h3>
+          </form>
+        </div>
+      </div>
+      <div className="">
+        <img src="/Images/Iaun.png" alt="" className="object-cover"/>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
