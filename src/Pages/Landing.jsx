8b09f2a46cad6eb8b652a71c2ddb71ba7bf8c7f4
Ali <aliazadigithub@gmail.com>
2026-04-19 16:12:53 +0330

    Add (Responsive): Landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -7,12 +7,12 @@ const Landing = () => {
     <div className="font-Vazir">
       <Header />
       <main>
-        <div className="">
-          <div className="flex items-start p-[20px] bg-gradient-to-r rounded-16">
+        <div>
+          <div className="flex flex-col max-w-[1200px] mx-auto tablet:flex-row items-center tablet:items-start p-[20px]rounded-16">
             <div className="flex flex-col items-center justify-between">
-              <div className="flex-center max-w-[600px] mx-auto">
-                <img src="./Images/Amoozeshyar_logo.png" alt="" />
-                <h1 className="text-center font-bold text-[24px] text-blue-500">
+              <div className="flex-center flex-col mobile:flex-row max-w-[600px] mx-auto">
+                <img src="./Images/Amoozeshyar_logo.png" alt="" className="w-[100px] mobile:w-[120px] tablet:w-[150px]"/>
+                <h1 className="text-center font-bold text-[24px] text-blue-500 m-[20px]">
                   سامانه مدیریت آموزشی یکپارچه دانشگاه آزاد اسلامی (آموزشیار)
                 </h1>
               </div>
@@ -22,7 +22,7 @@ const Landing = () => {
                 <Button text="ورود به سامانه سیما" />
               </div>
             </div>
-            <img src="./Images/Logo_1.png" alt="" />
+            <img src="./Images/Logo_1.png" alt="" className="max-w-[500px] w-full"/>
           </div>
           <img src="./Images/Logo_2.png" alt="" />
           <div className=" "></div>
